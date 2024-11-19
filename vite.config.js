5ff974d9bbd2d82411535ce7f6b814b076427e5a
ainajung <moonsi1verbin@gmail.com>
2024-11-19 20:44:23 +1100

    modified

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ['**/*.glb'],
-  base: '/Main-portfolio-site/'
+  assetsInclude: ['**/*.glb']
 })
